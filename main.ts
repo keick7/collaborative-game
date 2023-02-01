b19a8ced3292fb8d4eaf0e31af78c18451447803
keick7 <123963315+keick7@users.noreply.github.com>
2023-02-01 11:53:57 +0100

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -388,6 +388,8 @@ function playthrough() {
     jan.setPosition(117, 81)
     jan.setStayInScreen(true)
     controller.moveSprite(jan, 100, 0)
+    game.showLongText("Jan is an argumentative man from the grave diggers union who is searching for the perfect culinary recipe.", DialogLayout.Top)
+    game.showLongText("He is ready to begin work, press 'X' to interact with graves.", DialogLayout.Top)
     game.onUpdate(function testOverlap() {
         if (controller.B.isPressed() && jan.overlapsWith(falseGrave)) {
             game.showLongText("This grave doesn't seem promising. Keep looking.", DialogLayout.Top)
